85768d98eb1d8d0b70ab25c1abd498c713a628fd
jwpark <jiwoo9446@naver.com>
2023-05-30 13:22:55 +0900

    use select() for timer

--- task10/task10.c
+++ task10/task10.c
@@ -5,7 +5,7 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<fcntl.h>
-
+#include <syslog.h>
 int check_file(){
 	struct stat file;
 	if(stat("/tmp/test.txt", &file)==0){
@@ -18,7 +18,11 @@ int check_file(){
 int main(){
 	int check;
 	pid_t pid, sid;
-
+	fd_set rfds;
+	int retval;
+	struct timeval tv;
+	int fd;
+	openlog("daemon", LOG_PID, LOG_DAEMON);
 	if ((pid = fork())<0)
 		exit(1);
 
@@ -32,13 +36,26 @@ int main(){
 	umask(0);
 
 	while(1){
-		check = check_file();
-		if(check==0){
-		sleep(1);
-		printf("running...\n");
-		}else{
+		FD_ZERO(&rfds);
+		FD_SET(0,&rfds);
+
+		tv.tv_sec = 2;
+		tv.tv_usec = 0;
+
+		retval = select(1,&rfds,NULL,NULL,&tv);
+		if (retval == -1) {
+			perror("select failed");
 			break;
+		} else if (retval == 0) {
+			check = check_file();
+			if (check == 0) {
+				syslog(LOG_INFO, "File does not exist");
+			} else {
+				syslog(LOG_INFO, "File exists");
+				break;
+			}
 		}
+
 	}
 
 	printf("daemon end\n");	
